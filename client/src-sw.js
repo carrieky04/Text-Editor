ff128264b5feb162bbd3d60582d956e752b51378
Carrie Young <carrieky04@gmail.com>
2023-02-20 20:08:07 -0800

    adds service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  matchCallback,
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,10 @@ module.exports = () => {
       template: './index.html',
       title: 'Webpack Plugin',
     }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }), 
     ],
 
     module: {
