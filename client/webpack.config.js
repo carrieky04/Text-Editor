e0dc535eda11d43e8d46d0d3c33d02c81ea4aa20
Carrie Young <carrieky04@gmail.com>
2023-02-23 12:53:09 -0800

    adds manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,6 +29,22 @@ module.exports = () => {
       swSrc: './src-sw.js',
       swDest: 'src-sw.js',
     }), 
+    new WebpackPwaManifest({
+      name: 'jate',
+      short_name: 'jate',
+      description: 'Another text editor',
+      background_color: '#7eb4e2',
+      theme_color: '#7eb4e2',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
     ],
 
     module: {
