b39d598dfa607e5114615df892348e32c20ef062
Carrie Young <carrieky04@gmail.com>
2023-02-23 18:19:00 -0800

    fixes webpack manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,8 +35,8 @@ module.exports = () => {
       description: 'Another text editor',
       background_color: '#7eb4e2',
       theme_color: '#7eb4e2',
-      start_url: './',
-      publicPath: './',
+      start_url: '/',
+      publicPath: '/',
       icons: [
         {
           src: path.resolve('src/images/logo.png'),
